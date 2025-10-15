c1abaaaeda544ef5410d1f4e8d1406f60b6a2d12
Pragathi Sneha Palla <98396508+pallapragathi03@users.noreply.github.com>
2025-10-15 18:41:04 +0530

    Delete Machine Learning directory

--- Machine Learning/code6.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = ["A", "B", "C", "D", "E"]
-y = [3, 7, 8, 5, 4]
-
-plt.bar(x, y, color="orange")
-plt.title("Bar Chart Example")
-plt.show()
--- Machine Learning/code7.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-data = np.random.randn(1000)  # 1000 random numbers
-
-plt.hist(data, bins=30, color="purple", edgecolor="black")
-plt.title("Histogram Example")
-plt.show()
